3b8006a4c32a46441539c99509f15e0d3edd6938
pradeepnitsan <pradeep.nitsan@gmail.com>
2023-12-27 12:36:01 +0530

    [TASK] Store Log in Database and log file

--- Classes/Controller/ProductAreaController.php
+++ Classes/Controller/ProductAreaController.php
@@ -4,6 +4,15 @@ declare(strict_types=1);
 
 namespace NITSAN\NsT3dev\Controller;
 
+use Psr\Log\LoggerAwareInterface;
+use Psr\Log\LoggerAwareTrait;
+use TYPO3\CMS\Core\Authentication\BackendUserAuthentication;
+use TYPO3\CMS\Core\Database\ConnectionPool;
+use TYPO3\CMS\Core\Log\LogManager;
+use TYPO3\CMS\Core\SysLog\Action as SystemLogGenericAction;
+use TYPO3\CMS\Core\SysLog\Error as SystemLogErrorClassification;
+use TYPO3\CMS\Core\SysLog\Type as SystemLogType;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
 
 /**
  * This file is part of the "T3 Dev" Extension for TYPO3 CMS.
@@ -17,8 +26,14 @@ namespace NITSAN\NsT3dev\Controller;
 /**
  * ProductAreaController
  */
-class ProductAreaController extends \TYPO3\CMS\Extbase\Mvc\Controller\ActionController
+class ProductAreaController extends \TYPO3\CMS\Extbase\Mvc\Controller\ActionController implements LoggerAwareInterface
 {
+
+    use LoggerAwareTrait;
+
+    public function __construct() {
+        $this->logger = GeneralUtility::makeInstance(LogManager::class)->getLogger(__CLASS__);
+    }
 
     /**
      * productAreaRepository
@@ -43,7 +58,19 @@ class ProductAreaController extends \TYPO3\CMS\Extbase\Mvc\Controller\ActionCont
     public function listAction(): \Psr\Http\Message\ResponseInterface
     {
         $productAreas = $this->productAreaRepository->findAll();
-        $this->view->assign('productAreas', $productAreas);
+
+        if(count($productAreas) > 0){
+            $this->view->assign('productAreas', $productAreas);
+        } else {
+            $logTitle = 'Data Error';
+            $logMessage = 'Something went awry, No Data Found!';
+            $this->logger->warning($logMessage);
+            try {
+                // Write error message to sys_log table
+                $this->writeErrorLog($logMessage);
+            } catch (\Exception $exception) {
+            }
+        }
         return $this->htmlResponse();
     }
 
@@ -116,5 +143,46 @@ class ProductAreaController extends \TYPO3\CMS\Extbase\Mvc\Controller\ActionCont
         $this->addFlashMessage('The object was deleted. Please be aware that this action is publicly accessible unless you implement an access check. See https://docs.typo3.org/p/friendsoftypo3/extension-builder/master/en-us/User/Index.html', '', \TYPO3\CMS\Core\Messaging\AbstractMessage::WARNING);
         $this->productAreaRepository->remove($productArea);
         $this->redirect('list');
+    }
+
+    /**
+     * Writes an error in the sys_log table
+     *
+     * @param string $logMessage Default text that follows the message (in english!).
+     */
+    protected function writeErrorLog($logMessage)
+    {
+        $connection = GeneralUtility::makeInstance(ConnectionPool::class)
+            ->getConnectionForTable('sys_log');
+
+        if (!$connection->isConnected()) {
+            return;
+        }
+        $workspace = 0;
+        $userId = 0;
+        if(isset($GLOBALS['BE_USER']->user)){
+            $backendUser = $GLOBALS['BE_USER']->user;
+            if (isset($backendUser['uid'])) {
+                $userId = $backendUser['uid'];
+            }
+
+        }
+
+        $connection->insert(
+            'sys_log',
+            [
+                'userid' => $userId,
+                'type' => SystemLogType::ERROR,
+                'channel' => SystemLogType::toChannel(SystemLogType::ERROR),
+                'action' => SystemLogGenericAction::UNDEFINED,
+                'error' => SystemLogErrorClassification::SYSTEM_ERROR,
+                'level' => SystemLogType::toLevel(SystemLogType::ERROR),
+                'details_nr' => 0,
+                'details' => str_replace('%', '%%', $logMessage),
+                'IP' => (string)GeneralUtility::getIndpEnv('REMOTE_ADDR'),
+                'tstamp' => $GLOBALS['EXEC_TIME'],
+                'workspace' => $workspace,
+            ]
+        );
     }
 }
