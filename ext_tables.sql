405165407b04aae9cbd165d8e5f6d1152c140721
Divya Goklani <divya@nitsantech.com>
2023-12-28 12:30:27 +0530

    Add missing fields

--- ext_tables.sql
+++ ext_tables.sql
@@ -1,8 +1,11 @@
-CREATE TABLE tx_nst3dev_domain_model_productarea (
-	name varchar(255) NOT NULL DEFAULT '',
+CREATE TABLE tx_nst3dev_domain_model_productarea
+(
+	name        varchar(255) NOT NULL DEFAULT '',
+	image       int(11) unsigned NOT NULL DEFAULT '0',
 	description text,
+	slug        varchar(255) NOT NULL DEFAULT '',
+	type        varchar(255) NOT NULL DEFAULT '',
 );
-
 
 
 CREATE TABLE tx_nst3dev_domain_model_log
