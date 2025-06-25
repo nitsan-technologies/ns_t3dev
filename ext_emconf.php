fd3609af951a0c5eb8a179750230e01e59eaf5a4
NITSAN <sanjay@nitsan.in>
2025-06-25 15:54:46 +0530

    Update ext_emconf.php
    Update ext_emconf.php

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,11 +1,12 @@
 <?php
 
 $EM_CONF[$_EXTKEY] = [
-    'title' => 'T3 Dev',
-    'description' => 'Master piece of all extensions for the references',
+    'title' => 'T3 Dev – Developer Reference Extension',
+    'description' => 'A developer-centric TYPO3 extension built to serve as a reference for best practices and reusable code components during extension development.',
     'category' => 'example',
-    'author' => 'Nilesh Malankiya',
-    'author_email' => 'nilesh@nitsantech.com',
+    'author' => 'Team T3Planet',
+    'author_email' => 'info@t3planet.de',
+    'author_company' => 'T3Planet',
     'state' => 'experimental',
     'clearCacheOnLoad' => 0,
     'version' => '1.0.0',
